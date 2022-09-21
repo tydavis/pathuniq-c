167fe0fb19eb859b6ef0384f461edf187a5eb6df
Tyler Davis <tydavis@gmail.com>
2022-09-21 18:59:32 +0000

    Add termination if finished

--- pathuniq.c
+++ pathuniq.c
@@ -5,18 +5,20 @@
 int main(int argc, char *argv[]) {
   const char split[2] = ":";
   char *path_str, *sp;
-  size_t t = 0;
 
   path_str = getenv("PATH");
 
   if (path_str != NULL) {
+    size_t t = 0;
     size_t psize = strlen(path_str);
     // Create array size of existing path, so we have enough room
     char export_string[psize];
     // We memset contents of the array to the path-separator because if we
-    // don't, the array terminates on the first element on the split-path.
+    // don't, the array terminates with '\0' after the first element on the
+    // split-path.
     memset(export_string, ':', sizeof(export_string));
-    export_string[psize] = '\0'; // Set last size to NUL
+    // Set last entry to NUL to ensure we terminate the string
+    export_string[psize] = '\0';
 
     sp = strtok(path_str, split);
     while (sp != NULL) {
@@ -33,12 +35,15 @@ int main(int argc, char *argv[]) {
       // Move index the length of element (plus one for separator)
       t = t + l + 1;
 
-      // Note: this command increments the next section of the split path
+      // increment to the next section of the split path, or return NULL ptr
       sp = strtok(NULL, split);
+
+      if (sp == NULL) { // If we've reached the end, terminate the string
+        export_string[t - 1] = '\0';
+      }
     }
     printf("export PATH=%s\n", export_string);
-  } else {                                            // Path not found
-    printf("PATH environment variable not found!\n"); // DEBUG
+  } else { // Path not found
     exit(EXIT_FAILURE);
   }
   exit(EXIT_SUCCESS);
